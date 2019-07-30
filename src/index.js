34bee7f7e59a496616e2914ee147f6f5f72cf4ae
SUJI_HH94 <susie3765@naver.com>
2019-07-30 19:43:53 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,41 @@
+const array = [1, 2, 3, 4, 5, 6, 7, 8];
+
+const squred = [];
+
+// for(let i=0;i<array.length;i++){
+//   squred.push(array[i]*array[i]);
+// }
+
+array.forEach(n => {
+  squred.push(n * n);
+});
+
+console.log(squred);
+
+// 변수와 상수
+
+//변수 선언
+let value = 1;
+
+console.log(value);
+
+value = 2;
+console.log(value);
+
+//상수 선언
+const a = 1;
+console.log(a);
+
+//var << 키워드 쓰는 일이 없어짐
+
+let text = "hello";
+let name = "hih";
+
+let good = null;
+let up = undefined;
+
+let friend = null;
+let criminal;
+
+// 값을 지정하지 않음
+console.log(criminal);
